be974ef8949b7346b3962516a67106f95c14fa32
梦宇@极客学院 <xtiqin@163.com>
2015-07-27 18:17:08 +0800

    建立基本界面

---
+++ src/com/plter/lrceditor/controllers/LRCEditorController.java
@@ -0,0 +1,10 @@
+package com.plter.lrceditor.controllers;
+
+import javafx.scene.control.Label;
+
+/**
+ * Created by plter on 7/27/15.
+ */
+public class LRCEditorController {
+    public Label label;
+}
